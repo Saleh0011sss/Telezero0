623a9a025aa909ddb54db380f4fb799df773f0f9
Saleh0011 <salehfffaaa@gmail.com>
2016-11-01 23:18:23 +0330

    Update and rename telezeusbot.lua to seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -212,7 +212,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-	"admin",
+    "admin",
     "onservice",
     "inrealm",
     "ingroup",
@@ -231,24 +231,13 @@ function create_config( )
     "supergroup",
     "whitelist",
     "msg_checks"
+    "warn"
+    "up"
     },
     sudo_users = {188383245,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Telzeus v1
-An advanced administration bot based on TG-CLI written in Lua
-
-
-
-Admins
-@sudoAm [sudo]
-@imandaneshi [Developer]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+این سورس تله سید میباشد 
 ]],
     help_text_realm = [[
 Realm Commands:
