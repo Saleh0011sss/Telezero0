42b4c342d5a72cdc1b238e55055d4706e1ec97c4
Saleh0011 <salehfffaaa@gmail.com>
2016-11-01 23:03:46 +0330

    Update and rename seedbot.lua to telezeusbot.lua

--- bot/telezeusbot.lua
+++ bot/telezeusbot.lua
@@ -228,23 +228,20 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-	"supergroup",
-	"whitelist",
-	"msg_checks"
+    "supergroup",
+    "whitelist",
+    "msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {188383245,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[Telzeus v1
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
+
 
 Admins
-@iwals [Founder]
+@sudoAm [sudo]
 @imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
 
 Special thanks to
 awkward_potato
@@ -252,12 +249,6 @@ Siyanew
 topkecleon
 Vamptacus
 
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
 ]],
     help_text_realm = [[
 Realm Commands:
